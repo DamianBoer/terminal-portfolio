f17cb87ae90534b9c881cce69885da53c6c01082
Damian Boersma <boersmadamian@gmail.com>
2023-04-02 18:01:33 -0400

    inital commit

---
+++ js/commands.js
@@ -0,0 +1,46 @@
+var github = "https://github.com/DamianBoer";
+
+whois = [
+  "<br>",
+  "Hey, I'm Damian!👋",
+  "I'm a Current Highschool student studying to end up in the field of Computer Science",
+
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Damian?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+'									                ',
+' ██████╗  █████╗ ███╗   ███╗██╗ █████╗ ███╗   ██╗	',
+' ██╔══██╗██╔══██╗████╗ ████║██║██╔══██╗████╗  ██║	',
+' ██║  ██║███████║██╔████╔██║██║███████║██╔██╗ ██║	',
+' ██║  ██║██╔══██║██║╚██╔╝██║██║██╔══██║██║╚██╗██║	',
+' ██████╔╝██║  ██║██║ ╚═╝ ██║██║██║  ██║██║ ╚████║	',
+' ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝	',
+'                                               	',
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
